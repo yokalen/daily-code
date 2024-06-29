c4c3d5d1185c77fa62fc819e288d97b446b61e63
My Computer <yokalen@gmail.com>
2024-06-29 09:10:20 -0700

    four

--- js/solution.js
+++ js/solution.js
@@ -4,4 +4,9 @@
 // 2,6,2 => (2 + 4 + 6) => 12
 // 1,5,1 => (1 + 2 + 3 + 4 + 5) => 15
 // 1,5,3 => (1 + 4) => 5
+// 0,7,2 => (2 + 4 + 6) => 12
 
+const sequenceSum = (begin, end, step) => {
+  // starting with begin, create sequence by step until end
+  // reduce the sequence to sum
+};
