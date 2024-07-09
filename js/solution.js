5b3ba8d051ddbf109d20a0cb0f636aae0ba03409
My Computer <yokalen@gmail.com>
2024-07-09 15:19:01 -0700

    dev

--- js/solution.js
+++ js/solution.js
@@ -1,25 +1,19 @@
-// 3 num - begin, end, step
-// if begin > end => 0 : sum seq of nums 
-// 2,2,2 => 2
-// 2,6,2 => (2 + 4 + 6) => 12
-// 1,5,1 => (1 + 2 + 3 + 4 + 5) => 15
-// 1,5,3 => (1 + 4) => 5
-// 0,7,2 => (2 + 4 + 6) => 12
+// array of nums, target num
+// index of two nums whose sum is target
+// [1, 2, 3], 4 => 0, 2 // [3, 2, 4], 6 => 1, 2 // [7, 12, 40, 42, 44, 47], 86 => 3, 4
 
-const sequenceSum = (begin, end, step) => {
-  // starting with begin, create sequence by step until end
-  let sequence = []
-  
-  for (let i = begin; i <= end; i+= step){
-    sequence.push(i)
-  }
-  // reduce the sequence to sum
-  const result = sequence.reduce((a,b) => a + b, 0)
-  return result;
-};
-
-console.log(sequenceSum(2,2,2), 2)
-console.log(sequenceSum(2,6,2), 12)
-console.log(sequenceSum(1,5,1), 15)
-console.log(sequenceSum(1,5,3), 5)
-console.log(sequenceSum(0,7,2), 12)
+function twoSum(numbers, target) {
+  // starting with indx 0, add to each other num to find target, keep going until found
+  let gotIt;
+  for (let i = 0; i < numbers.length; i++) {
+    const onIt = numbers[i];
+    for (let j = 1; j < numbers.length; j++){
+      const check = number[j];
+      if(onIt + check === target){
+        gotIt = [i, j];
+      };
+    };
+  };
+  // return indx of num in loop and match
+  return gotIt;
+}
