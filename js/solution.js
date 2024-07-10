a7c80ac86ad69ca6c3ecf3cbe4b611249a9d418e
My Computer <yokalen@gmail.com>
2024-07-10 15:19:46 -0700

    code

--- js/solution.js
+++ js/solution.js
@@ -17,3 +17,7 @@ function twoSum(numbers, target) {
   // return indx of num in loop and match
   return gotIt;
 }
+// test
+console.log(twoSum([1, 2, 3], 4), "0, 2");
+console.log(twoSum([3, 2, 4], 6), "1, 2");
+console.log(twoSum([7, 12, 40, 42, 44, 47], 86), "3, 4");
