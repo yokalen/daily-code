894785321ec48e918ea5235264d48478a6eb0938
My Computer <yokalen@gmail.com>
2024-07-12 15:31:08 -0700

    clean

--- js/solution.js
+++ js/solution.js
@@ -3,20 +3,16 @@
 // [1, 2, 3], 4 => 0, 2 // [3, 2, 4], 6 => 1, 2 // [7, 12, 40, 42, 44, 47], 86 => 3, 4
 
 function twoSum(numbers, target) {
-  // starting with indx 0, add to each other num to find target, keep going until found
-  let gotIt;
   for (let i = 0; i < numbers.length; i++) {
     const onIt = numbers[i];
     for (let j = 1; j < numbers.length; j++){
-      const check = number[j];
+      const check = numbers[j];
       if(onIt + check === target){
-        gotIt = [i, j];
+        return [i, j];
       };
     };
   };
-  // return indx of num in loop and match
-  return gotIt;
-}
+};
 // test
 console.log(twoSum([1, 2, 3], 4), "0, 2");
 console.log(twoSum([3, 2, 4], 6), "1, 2");
